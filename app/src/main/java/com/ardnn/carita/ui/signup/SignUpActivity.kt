940b81668db93bc739d2695ac38ac4be6c38c999
muammarahlnn <21muammarahlan@gmail.com>
2022-09-29 02:01:12 +0800

    add sign up page UI

---
+++ app/src/main/java/com/ardnn/carita/ui/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.ardnn.carita.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ardnn.carita.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
