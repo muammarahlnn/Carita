1decd6c1b3f26428b3e3f68e9e0c794b480416a2
muammarahlnn <21muammarahlan@gmail.com>
2022-10-07 22:56:58 +0800

    add remote module

---
+++ app/src/main/java/com/ardnn/carita/data/di/RemoteModule.kt
@@ -0,0 +1,38 @@
+package com.ardnn.carita.data.di
+
+import com.ardnn.carita.data.main.repository.source.remote.StoryDicodingApi
+import com.ardnn.carita.data.util.ApiConst
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+@Module
+class RemoteModule {
+
+    @Provides
+    fun provideStoryDicodingApi(client: OkHttpClient): StoryDicodingApi =
+        buildRetrofit(client).create(StoryDicodingApi::class.java)
+
+    @Provides
+    fun provideOkhttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+
+    private fun buildRetrofit(client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(ApiConst.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(client)
+            .build()
+}
